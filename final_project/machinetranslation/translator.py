d5d8252589449ae6776b46ccf058242e6d247400
MuchCodin <96155379+MuchCodin@users.noreply.github.com>
2022-03-29 20:09:37 -0400

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,13 +9,13 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator('g33Q_travKou6zdN7w6q3pUyW1vgmxRqcg3EfDEfikRz')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator = authenticator
 )
 
-language_translator.set_service_url(url)
+language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/16448c74-65e5-4ebf-8bd0-77b09971f1a5')
 
 def english_to_french(english_text):
     #write the code here
